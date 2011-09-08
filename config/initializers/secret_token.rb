6145d383722001634a3e388d9a4806c5eda82105
Jan Krutisch <jan@krutisch.de>
2011-09-08 09:26:58 +0100

    prepping shirtbase for github publish

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Shirtbase::Application.config.secret_token = 'a5a0fd2e1758bffe703a82bca959bcd9d8e1ff1445a9eaf358ae84fbed67fc9fe29d95c3d69a5eeabeb8e93a16e75e4169480b4bcc29215918163db73385c045'
+Shirtbase::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN'] || 'a5a0fd2e1758bffe703a82bca959bcd9d8e1ff1445a9eaf358ae84fbed67fc9fe29d95c3d69a5eeabeb8e93a16e75e4169480b4bcc29215918163db73385c045'
